365f6c5ce71e7a412c1112ff8f3560f724dde920
Brian Zhang <bzhang@Mac-BrianZ.local>
2017-10-13 16:58:21 -0400

    merge overlay and svg

---
+++ resources/overlay.js
@@ -0,0 +1,7 @@
+function openOverlay() {
+    document.getElementById("qOverlay").style.height = "100%";
+}
+
+function closeOverlay() {
+    document.getElementById("qOverlay").style.height = "0%";
+}
