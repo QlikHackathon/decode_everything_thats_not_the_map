d3bed6596ee15778fc9d010e47aebcaa6395e886
Tuanhao <tuanhao256@gmail.com>
2017-10-14 15:55:43 -0400

    add animation for overlay content

--- resources/overlay.js
+++ resources/overlay.js
@@ -1,9 +1,11 @@
 function openOverlay() {
-    document.getElementById("qOverlay").style.height = "100%";
+    // document.getElementById("qOverlay").style.height = "100%";
+    // document.getElementById("qOverlay").style.animation = "example";
     document.getElementById("all_filters").style.display = "none";
 }
 
 function closeOverlay() {
-    document.getElementById("qOverlay").style.height = "0%";
+    // document.getElementById("qOverlay").style.height = "0%";
+    document.getElementById("qOverlay").style.animationName = "close-overlay";
     document.getElementById("all_filters").style.display = "inline";
 }
