6f14fccb398248d911ec79f229daeda4be566a69
Brian Zhang <brian.k.zhang@gmail.com>
2017-10-14 17:35:44 -0400

    Scroll in 2 (#36)
    * add animation for overlay content
    
    * Added new overlay bg image and header
    
    * question mark image
    
    * enhance the animation
    
    * reduce animation duration
    
    * fixing conflicts

--- resources/overlay.js
+++ resources/overlay.js
@@ -1,9 +1,13 @@
 function openOverlay() {
-    document.getElementById("qOverlay").style.height = "100%";
-    document.getElementById("filters-bar").style.display = "none";
+    document.getElementById("qOverlay").style.animationName = "open-overlay";
 }
 
 function closeOverlay() {
-    document.getElementById("qOverlay").style.height = "0%";
-    document.getElementById("filters-bar").style.display = "inline";
+    document.getElementById("qOverlay").style.animationName = "close-overlay";
 }
+
+$(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta < 0) {
+        closeOverlay();
+    }
+})
